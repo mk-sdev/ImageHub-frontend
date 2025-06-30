656612bb7fba719ec4a68257ac7416418f0839af
Mikołaj <kaleta222@gmail.com>
2025-06-30 13:59:02 +0200

    ui: set up auth form

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,10 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { Image } from 'expo-image'
+import { Platform, StyleSheet, TextInput } from 'react-native'
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { HelloWave } from '@/components/HelloWave'
+import ParallaxScrollView from '@/components/ParallaxScrollView'
+import { ThemedText } from '@/components/ThemedText'
+import { ThemedView } from '@/components/ThemedView'
 
 export default function HomeScreen() {
   return (
@@ -15,55 +15,59 @@ export default function HomeScreen() {
           source={require('@/assets/images/partial-react-logo.png')}
           style={styles.reactLogo}
         />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
+      }
+    >
+      <ThemedText>Login</ThemedText>
+      <TextInput
+        placeholder="enter email"
+        autoComplete="email"
+        style={styles.input}
+        placeholderTextColor="gray"
+      ></TextInput>
+      <TextInput
+        secureTextEntry={true}
+        placeholder="enter password"
+        style={styles.input}
+        placeholderTextColor="gray"
+      ></TextInput>
+
+      <ThemedView
+        style={{
+          // borderBottomColor: 'dimgray',
+          // borderBottomWidth: 1,
+          // paddingBottom: 5,
+          // width: '100%',
+          // height: 10,
+          marginTop: 30,
+          opacity: 0.5,
+        }}
+      >
+        <ThemedText style={{ textAlign: 'center' }}>OR</ThemedText>
       </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
+
+      <ThemedText>Register</ThemedText>
+      <TextInput
+        placeholder="enter email"
+        autoComplete="email"
+        style={styles.input}
+        placeholderTextColor="gray"
+      ></TextInput>
+      <TextInput
+        secureTextEntry={true}
+        placeholder="enter password"
+        style={styles.input}
+        placeholderTextColor="gray"
+      ></TextInput>
     </ParallaxScrollView>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  input: {
+    color: 'ivory',
+    borderWidth: 1,
+    borderColor: 'dimgray',
+    paddingLeft: 10
   },
   reactLogo: {
     height: 178,
@@ -72,4 +76,4 @@ const styles = StyleSheet.create({
     left: 0,
     position: 'absolute',
   },
-});
+})
